4289f064cf40ca97726895a48e56ea721d21f84f
sheetung <755855262@qq.com>
2025-02-09 17:52:48 +0800

    init

--- main.py
+++ main.py
@@ -1,48 +1,37 @@
 from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
 from pkg.plugin.events import *  # 导入事件类
-
+from pkg.platform.types import *
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class MyPlugin(BasePlugin):
-
+@register(name='KeywordTrigger', 
+          description='关键词触发插件', 
+          version='0.11', 
+          author="sheetung")
+class KeywordTriggerPlugin(BasePlugin):
     # 插件加载时触发
     def __init__(self, host: APIHost):
         pass
 
-    # 异步初始化
-    async def initialize(self):
-        pass
-
-    # 当收到个人消息时触发
-    @handler(PersonNormalMessageReceived)
-    async def person_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
-
-    # 当收到群消息时触发
-    @handler(GroupNormalMessageReceived)
+    @handler(PersonMessageReceived)
+    @handler(GroupMessageReceived)
     async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
+        msg = str(ctx.event.message_chain).strip()
+        # 检查是否包含关键词“云服务器”
+        if "云服务器" in msg:
+            sender_id = ctx.event.sender_id
+            # 构建要发送的消息
+            message = [
+                At(target=sender_id),  # At发送者
+                "\n云服务器9.9/年，只圈钱不跑路\n"
+            ]
+            # 添加图片
+            image_url = "https://static.moontung.top/2024/202409021559544.jpg"  # 替换为实际的图片URL
+            message.append(Image(url=image_url))
 
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
+            # 发送消息
+            await ctx.send_message(ctx.event.launcher_type, str(ctx.event.launcher_id), MessageChain(message))
             ctx.prevent_default()
+            ctx.prevent_postorder()
 
     # 插件卸载时触发
     def __del__(self):
