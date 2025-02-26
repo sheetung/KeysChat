db672e3a4658062b014736fa32a628405f187cb8
“sheetung” <755855262@qq.com>
2025-02-26 17:33:58 +0800

    json外置

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import json
 from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
 from pkg.plugin.events import *  # 导入事件类
 from pkg.platform.types import *
+import os
 
 # 注册插件
 @register(name='KeysChat', 
           description='关键词触发插件', 
-          version='0.12', 
+          version='0.13', 
           author="sheetung")
 class KeywordTriggerPlugin(BasePlugin):
     # 插件加载时触发
@@ -16,24 +18,13 @@ class KeywordTriggerPlugin(BasePlugin):
     @handler(GroupMessageReceived)
     async def group_normal_message_received(self, ctx: EventContext):
         msg = str(ctx.event.message_chain).strip()
-        # 检查是否包含关键词“云服务器”
         sender_id = ctx.event.sender_id
-
-        # 定义关键词与回复内容的映射表
-        keyword_responses = {
-            "云服务器": {
-                "message": "\n云服务器9.9/年，只圈钱不跑路\n",
-                "image_url": "https://static.moontung.top/2024/202409021559544.jpg"  # 替换为实际的图片URL
-            },
-            "优惠活动": {
-                "message": "优惠活动开始了，快来抢购！\n5.9/年\n",
-                "image_url": "https://static.moontung.top/2024/202409021559544.jpg" 
-            },
-            "客服": {
-                "message": "客服在线，欢迎咨询！",
-                "image_url": "" # 没有图片则为空
-            }
-        }
+        # print(f'ys={ctx.event.message_event}')
+        # 每次收到消息时加载 JSON 文件
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(script_dir, 'keyword_responses.json')
+        with open(file_path, 'r', encoding='utf-8') as f:
+            keyword_responses = json.load(f)
 
         # 检查消息中是否包含关键词
         for keyword, response in keyword_responses.items():
